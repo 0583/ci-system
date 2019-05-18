8c7b5349efae3f74c741ef932723f46045deb0ab
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-05-18 12:44:01 +0800

    [update] 修正 Jenkins 依赖。

--- script/init_jenkins.sh
+++ script/init_jenkins.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash 
 
+# 自动选择最快源
 yum install yum-fastestmirror -y
-#安装自动选择最快源的插件
 
-#添加Jenkins源
+# 添加 Jenkins 源
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://jenkins-ci.org/redhat/jenkins.repo
 
 sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 
+# Jenkins 依赖 Java
+yum install java
+
+# 安装 Jenkins
 yum install jenkins
-#安装jenkins
